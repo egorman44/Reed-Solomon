b2e152aff01eb33646ac5a511d33f41aee119b45
egorman44 <easmirnov44@gmail.com>
2025-04-25 16:28:19 +0400

    [Encoder] Update encoder_test.

--- coco_sim/encoder_test.py
+++ coco_sim/encoder_test.py
@@ -1,26 +1,38 @@
+from reedsolo import rs_encode_msg
+from reedsolo import init_tables
+
 from gf_arithmetic import GFArithmetic
 from rs_encoder import ReedSolomonEncoder
+from enc_msg_comparator import EncodedMessageComparator
+    
+FCR=0
+POLY=285
+N_LEN=240
+K_LEN=214
+SYMB_WIDTH=8
+REDUNDANCY=N_LEN-K_LEN
+
+init_tables()
+message = []
+for i in range(K_LEN):
+    message.append(i)
+
+enc_msg_reedsolo = list(rs_encode_msg(msg_in=message, fcr=FCR , nsym=REDUNDANCY))
 
 # Initialize Galois Field GF(2^3) with poly = x^3 + x + 1 => 0b1011
-gf = GFArithmetic(symb_width=3, poly=0b1011, generator=2)
+gf = GFArithmetic(symb_width=SYMB_WIDTH, poly=POLY, generator=2)
 
 # RS(7,3) means N=7, K=3 => 4 ECC symbols
-rs = ReedSolomonEncoder(gf=gf, N_LEN=7, K_LEN=3, bus_width = 3)
-
-# Define message using alpha exponents: [α^5, α^3, α^1]
-message = [
-    gf.alpha_to_symb(5),
-    gf.alpha_to_symb(3),
-    gf.alpha_to_symb(1),
-]
-
+rs = ReedSolomonEncoder(gf=gf, N_LEN=N_LEN, K_LEN=K_LEN, bus_width = 4, fcr=FCR)
+    
 # Encode the message
-encoded = rs.encode(message)
+enc_msg = rs.encode(message)
+enc_msg_seq = rs.encode_seq(message)
+enc_msg_par = rs.encode_par(message)
 
-lfsr = rs.evaluate_lfsr_states(message)
-
-# Display the result in symbol and alpha power formats
-print("Encoded message (symbols):", encoded)
-print("Encoded message (alpha exponents):", [gf.symb_to_alpha(symb) for symb in encoded])
-for i in range(len(lfsr)):
-    print(f"{i}: {lfsr[i]} = a^{gf.symb_to_alpha(lfsr[i])}")
+# Create comparator
+comparator = EncodedMessageComparator()
+#comparator.add_msg('encode', enc_msg)
+comparator.add_msg('encode_seq', enc_msg_seq)
+comparator.add_msg('encode_par', enc_msg_par )
+comparator.compare()
